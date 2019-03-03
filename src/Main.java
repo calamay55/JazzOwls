da773362e3cdedd01e0de06468f2b31df07aff8a
Alexander Sage <alexandersage@Dragon.local>
2019-03-03 13:52:33 -0500

    Fixed some typoes

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ public class Main {
 
     //y=(0.198 * (1 - (1 - (250 - x - y)/1000) ^20 ) + 0.056) * x + 0.60(1-y/500)^20 * y
 
+    //e^(20log(1-x)) = (1-x) ^ 20
+
     public static void main(String[] args){
-        for(double y=0; y<500; y++){
+        for(double y=-100; y<500; y++){
             getY(y);
         }
     }
+
 
     static double getX(double y){
         double yFraction = y/500;
@@ -31,8 +34,8 @@ public class Main {
         double yFraction = y/500;
         double xyTwentyPower = Math.pow(1-xyFraction, 20);
         double yTwentyPower = Math.pow(1-yFraction, 20);
-        double xFactor = 0.198 * (1-xyTwentyPower + 0.56);
-        double yFactor = 0.60 * yTwentyPower * y;
+        double xFactor = 0.198 * (1-xyTwentyPower) + 0.056;
+        double yFactor = 0.60 * yTwentyPower;
         double output = xFactor * x + yFactor * y;
         System.out.println(y + ", " + x + ", " + output);
         return output;
